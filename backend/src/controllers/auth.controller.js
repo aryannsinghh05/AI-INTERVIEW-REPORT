4a798652334bda1c2ece0208768f3157fd48da1d
Aryan <aryansinghh117@gmail.com>
2026-05-29 15:30:53 +0530

    feat: implement secure cross-origin cookie config with SameSite and HttpOnly flags for JWT auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -41,7 +41,12 @@ async function registerUserController(req,res){
         {expiresIn: "1d"}
     )
 
-    res.cookie("token" , token);
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+    });
 
     res.status(201).json({
         message: "User registered :)",
@@ -84,7 +89,12 @@ async function loginUserController(req,res){
         {expiresIn: "1d"}
     )
 
-    res.cookie("token" , token);
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+    });
 
     res.status(200).json({
         message: "LoggedIn success.",
@@ -110,7 +120,11 @@ async function logoutUserController(req,res){
         await BlacklistedTokenModel.create({token});
     }
 
-    res.clearCookie("token");
+    res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+    });
 
     res.status(200).json({
         message: "Logged out successfully :)"
